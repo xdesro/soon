a479c6e5d8107fd67ef15876e0467b9d134a2358
Henry Desroches <yo@henry.codes>
2021-02-05 18:54:09 -0700

    🎥  - Animation and interaction CSS start

--- src/js/index.js
+++ src/js/index.js
@@ -1,1 +1,15 @@
 console.log('test');
+const toggleDarkMode = ({ key }) => {
+  if (key == 'd') document.documentElement.toggleAttribute('dark');
+};
+const updateMousePosition = ({ clientX, clientY }) => {
+  document.documentElement.style.setProperty('--mouseX', `${clientX}px`);
+  document.documentElement.style.setProperty('--mouseY', `${clientY}px`);
+};
+
+document.addEventListener('keypress', (e) => {
+  toggleDarkMode(e);
+});
+document.addEventListener('mousemove', (e) => {
+  updateMousePosition(e);
+});
